da0d0fb7595e8dd8493c89153fd8038dfd96540c
NgoHuy05 <160807769+NgoHuy05@users.noreply.github.com>
2024-05-21 20:42:45 +0700

    Update Bullet.cpp

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -1,25 +1,17 @@
-// Định nghĩa lớp Bullet
 class Bullet {
 private:
-    int x; // Tọa độ x của đạn trên màn hình
-    int y; // Tọa độ y của đạn trên màn hình
-    int speed; // Tốc độ di chuyển của đạn
-
+    int x; 
+    int y; 
+    int speed; 
 public:
-    // Constructor khởi tạo vị trí và tốc độ ban đầu của đạn
     Bullet(int initialX, int initialY, int initialSpeed)
         : x(initialX), y(initialY), speed(initialSpeed) {}
-
-    // Phương thức di chuyển đạn
     void move() {
-        x += speed; // Di chuyển theo chiều dương của trục x (phía bên phải)
+        x += speed; 
     }
-    // Getter cho tọa độ x của đạn
     int getX() const {
         return x;
     }
-
-    // Getter cho tọa độ y của đạn
     int getY() const {
         return y;
     }

11602e845f1ee6b13bda4910b14cc1a16524e214
NgoHuy05 <160807769+NgoHuy05@users.noreply.github.com>
2024-05-23 16:05:28 +0700

    Update Threat.cpp

---
+++ src/Threat.cpp
@@ -0,0 +1,24 @@
+class Threat{
+private:
+    int x;
+    int y;
+    int speed;
+public:
+    Threat(int initialX, int initialY, int initialSpeed)
+        : x(initialX), y(initialY), speed(1.5) {}
+
+    void move() {
+        x -= speed;
+    }
+
+    int getX() const {
+        return x;
+    }
+
+    int getY() const {
+        return y;
+    }
+    bool isOffScreen() const {
+        return x < 0;
+    }
+};

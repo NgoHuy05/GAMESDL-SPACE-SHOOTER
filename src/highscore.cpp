2092b15aaa39e6597da54fe0146c4f36516ea37c
NgoHuy05 <160807769+NgoHuy05@users.noreply.github.com>
2024-05-23 10:20:14 +0700

    Add files via upload

---
+++ src/highscore.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+
+
+int readHighScore(const std::string& filename) {
+    std::ifstream file(filename);
+    int highScore = 0;
+    if (file.is_open()) {
+        file >> highScore;
+        file.close();
+    }
+    return highScore;
+}
+
+void writeHighScore(const std::string& filename, int highScore) {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        file << highScore;
+        file.close();
+    }
+}

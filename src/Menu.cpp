19aae69a43dd0763968f6d68fd9c1b31a18d6361
NgoHuy05 <160807769+NgoHuy05@users.noreply.github.com>
2024-05-27 18:05:44 +0700

    Update Menu.cpp

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,24 +1,7 @@
-void countdown(SDL_Renderer* renderer, TTF_Font* font) {
-    int countdown = 3;
-    Mix_Chunk* clickSound = Mix_LoadWAV("sound/click.mp3");
-    Mix_Chunk* tingSound = Mix_LoadWAV("sound/ting.mp3");
-    while (countdown > 0) {
-        Mix_PlayChannel(-1, clickSound, 0);
-        SDL_RenderClear(renderer);
-        std::string countdownText = std::to_string(countdown);
-        renderText(renderer, font, countdownText, 500, 250);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(1000);
-        countdown--;
-    }
-        Mix_PlayChannel(-1, tingSound, 0);
-}
-
 bool isMouseOverButton(int mouseX, int mouseY, const SDL_Rect& buttonRect) {
     return (mouseX >= buttonRect.x && mouseX <= (buttonRect.x + buttonRect.w) &&
             mouseY >= buttonRect.y && mouseY <= (buttonRect.y + buttonRect.h));
 }
-
 bool showMenu(SDL_Renderer* renderer, TTF_Font* font) {
         SDL_Surface* background2Surface = IMG_Load("image/background2.png");
         SDL_Texture* background2Texture = SDL_CreateTextureFromSurface(renderer, background2Surface);
@@ -101,7 +84,7 @@ bool showMenu(SDL_Renderer* renderer, TTF_Font* font) {
             renderText(renderer, font, "If you let the enemy cross the boundary, you will lose 1 HP", 100, 400);
             renderText(renderer, font, "If you collide with the enemy, you will lose 1 HP", 100, 450);
             renderText(renderer, font, "If you run out of HP, you will lose", 100, 500);
-             renderText(renderer, font, "Tip: Try to eliminate all enemies, don't let any of them pass, and avoid colliding with the enemies", 100, 350);
+            renderText(renderer, font, "Tip: Try to eliminate all enemies, don't let any of them pass, and avoid colliding with the enemies", 100, 350);
 
             SDL_RenderPresent(renderer);
 
@@ -139,4 +122,74 @@ bool showMenu(SDL_Renderer* renderer, TTF_Font* font) {
         }
     }
     return startGame;
+}
+
+bool showPauseMenu(SDL_Renderer* renderer, TTF_Font* font) {
+    SDL_Surface* backgroundSurface = IMG_Load("image/background.png");
+    SDL_Texture* backgroundTexture = SDL_CreateTextureFromSurface(renderer, backgroundSurface);
+    SDL_FreeSurface(backgroundSurface);
+
+    SDL_Surface* exit2Surface = IMG_Load("image/exit2.png");
+    SDL_Texture* exit2Texture = SDL_CreateTextureFromSurface(renderer, exit2Surface);
+    SDL_FreeSurface(exit2Surface);
+
+    SDL_Surface* continueSurface = IMG_Load("image/continue.png");
+    SDL_Texture* continueTexture = SDL_CreateTextureFromSurface(renderer, continueSurface);
+    SDL_FreeSurface(continueSurface);
+
+    SDL_Event event;
+    bool continueGame = false;
+    Mix_Chunk* clickSound = Mix_LoadWAV("sound/click.mp3");
+
+    while (!continueGame) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                exit(0);
+            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY);
+                SDL_Rect continueButtonRect = { 200, 300, 250, 80 };
+                SDL_Rect exitButtonRect = { 500, 300, 250, 80 };
+
+                if (isMouseOverButton(mouseX, mouseY, continueButtonRect)) {
+                    continueGame = true;
+                } else if (isMouseOverButton(mouseX, mouseY, exitButtonRect)) {
+                    Mix_PlayChannel(-1, clickSound, 0);
+                    showMenu(renderer, font);
+                }
+            }
+        }
+
+        int mouseX, mouseY;
+        SDL_GetMouseState(&mouseX, &mouseY);
+        bool mouseOverContinueButton = isMouseOverButton(mouseX, mouseY, {200, 300, 250, 80});
+        bool mouseOverExit2Button = isMouseOverButton(mouseX, mouseY, {500, 300, 250, 80});
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
+
+        SDL_Rect continueRect = { 200, 300, 250, 80 };
+        if (mouseOverContinueButton) {
+            SDL_SetTextureColorMod(continueTexture, 255, 255, 100);
+        } else {
+            SDL_SetTextureColorMod(continueTexture, 255, 255, 255);
+        }
+        SDL_RenderCopy(renderer, continueTexture, NULL, &continueRect);
+
+        SDL_Rect exit2Rect = { 500, 300, 250, 80 };
+        if (mouseOverExit2Button) {
+            SDL_SetTextureColorMod(exit2Texture, 255, 255, 100);
+        } else {
+            SDL_SetTextureColorMod(exit2Texture, 255, 255, 255);
+        }
+        SDL_RenderCopy(renderer, exit2Texture, NULL, &exit2Rect);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(backgroundTexture);
+    SDL_DestroyTexture(exit2Texture);
+    SDL_DestroyTexture(continueTexture);
+    Mix_FreeChunk(clickSound);
+    return continueGame;
 }
